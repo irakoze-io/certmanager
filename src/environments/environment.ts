ed551b305ff2f3a926be3e1d772e23edda665048
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-13 04:49:45 +0200

    Updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // In development we use same-origin (relative) URLs.
   // `ng serve` uses `proxy.conf.json` to forward /api and /auth to the backend.
-  apiUrl: 'https://identifies-necessarily-ambien-casa.trycloudflare.com',
+  apiUrl: 'https://walking-decrease-public-recognized.trycloudflare.com',
   apiBasePath: '/api',
   authBasePath: '/auth'
 };
