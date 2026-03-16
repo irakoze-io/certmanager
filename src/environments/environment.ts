b39683a26b8ff6bb91ad00510cb21426881cfb3f
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2026-03-17 01:34:16 +0200

    Updates on the backend configurations and the version of node and angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // In development we use same-origin (relative) URLs.
   // `ng serve` uses `proxy.conf.json` to forward /api and /auth to the backend.
-  apiUrl: 'https://florida-shopzilla-never-allen.trycloudflare.com',
+  apiUrl: 'http://localhost:8080',
   apiBasePath: '/api',
   authBasePath: '/auth'
 };
